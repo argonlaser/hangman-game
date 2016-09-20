9f2994ff4029f3a0539de74cbd4b80985d3e4fff
Venkata krishna <vkvenkat94@gmail.com>
2016-09-20 16:06:12 +0530

    Cleanups

--- game/Hangman.js
+++ game/Hangman.js
@@ -1,32 +1,23 @@
 'use strict'
 const parseJson = require('../parseJson.js')
-const Colors = require('colors')
-const keypress = require('keypress')
-const Table = require('cli-table')
-const clearCli = require('cli-clear')
 const fs = require('fs')
-
-Colors.setTheme({
-  success: 'green',
-  b: 'bold',
-  fail: 'red'
-})
+const utils = require('./utils')
 
 class HangmanGame {
   constructor (gameOptions) {
     this.chances = 7
     this.gameState = -1
     this.currentScore = 0
+    this.guessed = []
+    this.missed = []
     this.resultFile = gameOptions.resultFile
     this.hangmanWord = this.getRandomHangmanWord(gameOptions.datasrcFile)
-    this.initIOStream(gameOptions.input, gameOptions.output)
     this.initHangmanTable()
-    this.initHighScore(this.resultFile)
-    console.log('word is ' + JSON.stringify(this.hangmanWord))
+    this.initHighScore(gameOptions.resultFile)
   }
 
   initHangmanTable () {
-    this.tableContent = this._initialiseHangmanWordSlot(this.hangmanWord.term.length, '_')
+    this.tableContent = utils.initialiseHangmanWordSlot(this.hangmanWord.term.length, '_')
   }
 
   getRandomHangmanWord (fileName) {
@@ -34,23 +25,8 @@ class HangmanGame {
     return wordsJson[Math.floor(Math.random() * wordsJson.length)]
   }
 
-  initIOStream (input, output) {
-    this.input = input
-    this.output = output
-    this.input.setRawMode(true)
-    keypress(this.input)
-  }
-
   initHighScore (resultFile) {
     this.highestScore = this.readHighestScore(resultFile)
-  }
-
-  charPos (str, char) {
-    return str.split('').map(function (c, i) {
-      if (c === char) return i
-    }).filter(function (v) {
-      return v >= 0
-    })
   }
 
   readHighestScore (resultFile) {
@@ -62,17 +38,7 @@ class HangmanGame {
     }
   }
 
-  clear () {
-    clearCli()
-  }
-
-  _initialiseHangmanWordSlot (size, value) {
-    const array = []
-    while (size--) array[size] = value
-    return array
-  }
-
-  getResult () {
+  getGameState () {
     const chances = this.chances
     const tableContent = this.tableContent
     if (tableContent.indexOf('_') <= -1 && chances >= 0) {
@@ -82,11 +48,12 @@ class HangmanGame {
     if (chances <= 0) {
       this.gameState = 0
     }
-    //    console.log('Get Result :' + tableContent)
-    //    console.log('Get Result Index :' + tableContent.indexOf('_'))
-    //    console.log('Get result chances :' + chances)
-    //    console.log('Game State :' + this.gameState)
+
     return this.gameState
+  }
+
+  getScore () {
+    return this.duration
   }
 
   saveScore () {
@@ -99,84 +66,59 @@ class HangmanGame {
     }
   }
 
-  render () {
-    const self = this
-    const hangmanTerm = self.hangmanWord.term
-    self.clear()
-
-    const table = new Table({
-      colWidths: this._initialiseHangmanWordSlot(hangmanTerm, 3)
-    })
-    table.push(self.tableContent)
-    const result = self.getResult()
-    console.log(table.toString())
-    console.log('MEANING: ' + self.hangmanWord.definition)
-    switch (result) {
-      case 1:
-        self.output.write('\nYou won'.success)
-        this.saveScore()
-        process.exit(0)
-        break
-      case 0:
-        self.output.write('\nGame over !'.fail)
-        process.exit(0)
-        break
-    }
-  }
-
   guessKey (keyGuessed) {
     var self = this
     const hangmanTerm = self.hangmanWord.term
     if (!hangmanTerm.includes(keyGuessed)) {
-      self.chances--
+      if (self.missed.indexOf(keyGuessed) <= -1) {
+        self.missed.push(keyGuessed)
+        self.chances--
+      }
+
       if (self.chances <= 0) {
         self.tableContent = hangmanTerm.split('')
       }
 
       return false
     } else {
-      const indicesFound = self.charPos(hangmanTerm, keyGuessed)
+      const indicesFound = utils.charPos(hangmanTerm, keyGuessed)
       indicesFound.forEach(function (index) {
-        self.tableContent[index] = keyGuessed.success
+        self.tableContent[index] = keyGuessed
+        if (self.guessed.indexOf(keyGuessed) <= -1) {
+          self.guessed.push(keyGuessed)
+        }
       })
+
 
       return true
     }
   }
 
-  start () {
-    const self = this
-    const singleAlphaRegex = /^[a-zA-Z]$/
-    this.startTime = Date.now()
-    self.render()
-    self.input.on('keypress', function (ch, key) {
-      if (!key || !singleAlphaRegex.test(key.name.toString())) {
-        return
-      }
-
-      self.guessKey(key.name)
-      self.render()
-    })
+  init () {
+    return this.getDetails()
   }
 
-  nextTurn (key, correctGuess, wrongGuess) {
+  getDetails () {
+    return {
+      tableContent : this.tableContent,
+      hangmanWord : this.hangmanWord,
+      gameState : this.getGameState(),
+      guessed : this.guessed,
+      missed : this.missed,
+      chances : this.chances
+    }
+  }
+
+  nextTurn (key) {
     const self = this
     const singleAlphaRegex = /^[a-zA-Z]$/
 
-    if (!key || !singleAlphaRegex.test(key)) {
-      return
+    if (!singleAlphaRegex.test(key)) {
+      return self.getDetails()
     }
 
     const isRightGuess = self.guessKey(key)
-    if (isRightGuess) {
-      if (typeof correctGuess === 'function') {
-        correctGuess()
-      }
-    } else {
-      if (typeof wrongGuess === 'function') {
-        wrongGuess()
-      }
-    }
+    return self.getDetails()
   }
 }
 
--- index.js
+++ index.js
@@ -3,8 +3,12 @@
 
 const HangmanGame = require('./game/Hangman')
 const program = require('commander')
+const keypress = require('keypress')
 const gameOptions = require('./game/config')
+const ConsoleUI = require('./game/consoleUI')
+const consoleUI = new ConsoleUI(process.stdin, process.stdout)
 let game = null
+let gameDetails = null
 
 program
   .version('0.0.1')
@@ -20,23 +24,27 @@ if (program.freq) {
   // Default is high frequency game
   game = new HangmanGame(gameOptions[0])
 }
-
 // game.start()
 
+console.log(game.init())
+consoleUI.render(game.init())
+
+keypress(process.stdin)
+process.stdin.setRawMode('true')
+
 process.stdin.on('keypress', function (ch, key) {
+  if(!key || !key.name) {
+    return
+  }
+
   if (key && key.ctrl && key.name === 'c') {
     process.exit(0)
   }
 
-  game.nextTurn(
-    key.name,
-    function () {
-      // correct Guess
-      console.log('render for correct guess')
-    },
-    function () {
-      // wrong Guess
-      console.log('render for wrong guess')
-    }
-  )
+  gameDetails = game.nextTurn(key.name)
+  consoleUI.render(gameDetails)
+
+  if(gameDetails.gameState !== -1) {
+    process.exit(0);
+  }
 })
